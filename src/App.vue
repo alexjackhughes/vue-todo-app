e7a3e53ea5afaae4e64874b8e02243923e40e6cc
alexjackhughes <info@alexjackhughes.com>
2019-08-30 14:00:28 +0100

    App now correctly renders todos

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,22 @@
   <div id="app">
     <h1 class="title is-1">Super Fun Times</h1>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div v-for="category in categoryTypes" :key="category.id" class="flex">
+      <h3 class="title is-3">{{category.title}}</h3>
+      <p class="subtitle">{{category.description}}</p>
+      <font-awesome-icon class="addTodo" icon="plus-circle" />
 
-    <!-- Remember that todos is the name of our array -->
-    <ul>
-      <li v-for="todo in todos" v-bind:key="todo.id" class="flex">
-        <!-- and now, we can access to an item using todo-->
-        {{todo.title}}
-        <font-awesome-icon icon="star" />
-        <font-awesome-icon icon="plus-circle" />
-      </li>
-    </ul>
+      <ul>
+        <li v-for="todo in todos" :key="todo.id" class="flex">
+          <!-- and now, we can access to an item using todo-->
+          <div v-if="category.id === todo.category">
+            {{todo.title}}
+            <font-awesome-icon :class="todo.priority? 'priority': 'not-priority'" icon="star" />
+          </div>
+          <!-- <div v-else></div> -->
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -24,28 +30,41 @@ export default {
   data: function() {
     return {
       newTodoText: "",
+      categoryTypes: [
+        { id: "MEH", title: "Meh", description: "Just ignore this stuff" },
+        {
+          id: "IMPORTANT",
+          title: "Important",
+          description: "Like, kinda important stuff"
+        },
+        {
+          id: "LIFE_CHANGING",
+          title: "Life Changing",
+          description: "Dude, you have to do this stuff"
+        }
+      ],
       todos: [
         {
           id: 0,
-          title: "I am a todo",
+          title: "I am also life changing todo",
           priority: true,
           category: "LIFE_CHANGING"
         },
         {
           id: 1,
-          title: "I am a second todo",
+          title: "I am a life changing todo",
           priority: false,
           category: "LIFE_CHANGING"
         },
         {
           id: 2,
-          title: "I am a nice todo",
-          priority: true,
+          title: "I am an important todo",
+          priority: false,
           category: "IMPORTANT"
         },
         {
           id: 3,
-          title: "I am a cross todo",
+          title: "I am a meh todo",
           priority: true,
           category: "MEH"
         }
@@ -59,12 +78,23 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+.addTodo {
+  color: #ff271c;
+}
+
+.priority {
+  color: #ffe20a;
+}
+
+.not-priority {
+  color: #c7c7c7;
 }
 </style>
