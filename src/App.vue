e70442fa3fce29854d26bb9871598cda4d7c83d0
alexjackhughes <info@alexjackhughes.com>
2019-08-30 13:28:17 +0100

    Added Font Awesome for icons

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,66 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HELLO</h1>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <!-- Remember that todos is the name of our array -->
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo.id" class="flex">
+        <!-- and now, we can access to an item using todo-->
+        {{todo.title}}
+        <font-awesome-icon icon="star" />
+        <font-awesome-icon icon="plus-circle" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: "app",
+  data: function() {
+    return {
+      newTodoText: "",
+      todos: [
+        {
+          id: 0,
+          title: "I am a todo",
+          priority: true,
+          category: "LIFE_CHANGING"
+        },
+        {
+          id: 1,
+          title: "I am a second todo",
+          priority: false,
+          category: "LIFE_CHANGING"
+        },
+        {
+          id: 2,
+          title: "I am a nice todo",
+          priority: true,
+          category: "IMPORTANT"
+        },
+        {
+          id: 3,
+          title: "I am a cross todo",
+          priority: true,
+          category: "MEH"
+        }
+      ]
+    };
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
